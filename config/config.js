bf4585fc076f970e8cb03d7431b93167d1ee48d8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-19 23:14:34 +0200

    refactor: move global variables to config file

---
+++ config/config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const UPLOAD_FOLDER = process.env.UPLOAD_FOLDER || 'public';
+const AUTH_READ = process.env.AUTH_READ === 'yes';
+const AUTH_WRITE = process.env.AUTH_WRITE !== 'no';
+const SECRET_KEY = process.env.SECRET_KEY || 'Annie is Vader';
+const JWT_EXPIRATION = process.env.JWT_EXPIRATION || '24h';
+const PORT = process.env.PORT || 8000;
+const SALT = bcrypt.genSaltSync(process.env.SALT || 10);
+
+const dbFileName = process.env.DB_FILE || 'db.json';
+const dbFilePath = path.join(__dirname, dbFileName);
+
+const config = {
+  UPLOAD_FOLDER,
+  AUTH_READ,
+  AUTH_WRITE,
+  SECRET_KEY,
+  JWT_EXPIRATION,
+  PORT,
+  SALT,
+  dbFileName,
+  dbFilePath,
+};
+
+module.exports = config;
