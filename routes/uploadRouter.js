b947a6411858b251659eedebb03fbc31abc00470
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-20 11:13:14 +0200

    feat: use checkAuth and multer middlewares in upload route

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const uploadController = require('../controllers/uploadController');
 const checkAuth = require('../middleware/checkAuthMiddleware');
+const multerMiddleware = require('../middleware/multerMiddleware');
 const router = express.Router();
 
-router.post('/', checkAuth, uploadController);
+router.post(
+  '/upload',
+  checkAuth,
+  multerMiddleware().single('file'),
+  uploadController
+);
 
 module.exports = router;
