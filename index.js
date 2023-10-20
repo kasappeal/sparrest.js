a4555e52136f906615be8e4753c3d17f446540da
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-20 12:59:18 +0200

    feat: dinamically add port to swagger documentation server

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const cors = require('cors');
 const jsonServer = require('json-server');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const fs = require('fs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const initDB = require('./database/initDB');
 const config = require('./config');
@@ -21,6 +25,16 @@ server.use(cors());
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
+server.get('/', (req, res) => {
+  res.send('<h1>Sparrest API</h1><a href="/api/docs">Documentation</a>');
+});
+
+const { servers } = swaggerDocument;
+if (servers && servers.length > 0) {
+  servers[0].url = `http://localhost:${config.PORT}`;
+}
+
+server.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 server.use('/users', usersRouter);
 server.use('/auth', authRouter);
 server.use(/^\/api/, uploadRouter);
