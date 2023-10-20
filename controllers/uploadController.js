42f88a5ba256bd2116595a1ad3417486b5f6059a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-20 11:15:42 +0200

    feat: return file path in uploadController when a file is uploaded

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,25 +1,12 @@
-const multer = require('multer');
-const config = require('../config');
-const storage = require('../middleware/multerMiddleware');
+const path = require('path');
 
 const uploadFile = (req, res) => {
-  const upload = multer({ storage }).single('file');
-  upload(req, res, function (err) {
-    if (req.fileValidationError) {
-      return res.status(400).json({ message: req.fileValidationError });
-    } else if (!req.file) {
-      return res.status(400).json({ message: 'file field is required' });
-    } else if (err instanceof multer.MulterError) {
-      return res.status(400).json({ message: err });
-    } else if (err) {
-      return res.status(400).json({ message: err });
-    }
-    const path = `${req.protocol}://${req.get('host')}/${req.file.path
-      .replace(config.UPLOAD_FOLDER, '')
-      .replace('\\', '/')
-      .replace('//', '/')}`;
-    return res.status(201).json({ path });
-  });
+  const filePath = path.posix.join(
+    'uploads',
+    req.user.username,
+    req.file.filename
+  );
+  res.status(201).json({ path: filePath });
 };
 
 module.exports = uploadFile;
