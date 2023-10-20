93970086d96db869f3f835a48bd2c8ecf8384ba9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-20 17:45:50 +0200

    feat: use fileValidationMiddleware in uploadRouter

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -2,12 +2,14 @@ const express = require('express');
 const uploadController = require('../controllers/uploadController');
 const checkAuthMiddleware = require('../middleware/checkAuthMiddleware');
 const multerMiddleware = require('../middleware/multerMiddleware');
+const fileValidationMiddleware = require('../middleware/fileValidationMiddleware');
 const router = express.Router();
 
 router.post(
   '/upload',
   checkAuthMiddleware,
   multerMiddleware().single('file'),
+  fileValidationMiddleware,
   uploadController
 );
 
