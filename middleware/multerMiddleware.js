8d1a4cd74daa1d67920d31761869b55ffd3286c0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-17 17:29:18 +0100

    refactor: multerMiddleware now returns multer({storage}) directly

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -2,23 +2,21 @@ const multer = require('multer');
 const config = require('../config');
 const path = require('path');
 
-const fileUpload = () => {
-  const storage = multer.diskStorage({
-    destination: async (req, file, cb) => {
-      const newPath = path.posix.join(
-        __dirname,
-        config.UPLOAD_FOLDER,
-        req.user.username
-      );
-      cb(null, newPath);
-    },
-    filename: (_req, file, cb) => {
-      const fileName = Date.now() + file.originalname;
-      cb(null, fileName);
-    },
-  });
+const storage = multer.diskStorage({
+  destination: async (req, file, cb) => {
+    const newPath = path.posix.join(
+      __dirname,
+      config.UPLOAD_FOLDER,
+      req.user.username
+    );
+    cb(null, newPath);
+  },
+  filename: (_req, file, cb) => {
+    const fileName = Date.now() + file.originalname;
+    cb(null, fileName);
+  },
+});
 
-  return multer({ storage });
-};
+const fileUpload = multer({ storage });
 
 module.exports = fileUpload;
